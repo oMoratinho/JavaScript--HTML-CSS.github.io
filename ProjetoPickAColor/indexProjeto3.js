ca23064f568a4911d1ba580e2e0487e7055ee644
Gabriel Morato <gabriel01.wheels@gmail.com>
2024-06-10 09:48:50 -0300

    Pick a Color
    Pick a Color project with JS, HTML & CSS

---
+++ ProjetoPickAColor/indexProjeto3.js
@@ -0,0 +1,18 @@
+
+
+const colors = ['yellow', 'red', 'green', 'blue', 'purple']
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+
+btn.addEventListener('click', function(){
+    const randomColor = getRandomColor()
+    document.body.style.background = colors[randomColor]
+    color.textContent= colors[randomColor]
+})
+
+function getRandomColor() {
+    return Math.floor(Math.random() * colors.length)
+}
+
+
